66a5bdd5d1645437b9bfb40ba902b8db01c16adf
Bikram Bam <s310280@oslomet.no>
2018-10-29 23:59:25 +0100

    my test

---
+++ count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+from itertools import islice
+
+count=0.0
+
+mystring="better"
+with open(sys.argv[1], 'r') as myfile:
+ for line in islice(myfile,1,10000):
+   #if mystring in line.split():
+        count=count+line.count("better")
+print mystring,float(count) 
+
