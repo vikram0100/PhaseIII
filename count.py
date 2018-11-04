25dcf720888c15781bde68616f78848cd057174b
Bikram Bam <s310280@oslomet.no>
2018-11-05 00:51:02 +0100

    Researcher B version of code

--- count.py
+++ count.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import sys
 from itertools import islice
-count=0
+total=0
 mystring="better"
 with open(sys.argv[1], 'r') as myfile:
  for line in islice(myfile,1,10000):
-   #if mystring in line.split():
-        count=count+line.count("better")
-print count 
+   if mystring in line.split():
+        total=total+1
+print total
 
