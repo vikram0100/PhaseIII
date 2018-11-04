6a9d922313a52404e1a91d2c14e4f3e01c344e6a
Bikram Bam <s310280@oslomet.no>
2018-11-05 00:06:54 +0100

    researcher A version of Script

--- count.py
+++ count.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import sys
 from itertools import islice
-
-count=0.0
-
+count=0
 mystring="better"
 with open(sys.argv[1], 'r') as myfile:
  for line in islice(myfile,1,10000):
    #if mystring in line.split():
         count=count+line.count("better")
-print float(count) 
+print count 
 
