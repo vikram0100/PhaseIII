86e5887182914f06e152ba20bbdfa4617def2761
vikram0100 <bikram_bom@yahoo.com>
2019-01-18 01:21:05 +0000

    researcher B script

--- count.py
+++ count.py
@@ -7,7 +7,7 @@ count=0.0
 mystring="better"
 with open(sys.argv[1], 'r') as myfile:
  for line in islice(myfile,1,10000):
-   #if mystring in line.split():
+   if mystring in line.split():
         count=count+line.count("better")
 print float(count) 
 
