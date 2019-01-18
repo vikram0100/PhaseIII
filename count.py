83f43873a9e289a5183fb4faf055837dec15d7a5
bikram <bikram_bom@yahoo.com>
2019-01-18 01:49:28 +0000

    researcher a code

--- count.py
+++ count.py
@@ -8,13 +8,13 @@ mystring="better"
 with open(sys.argv[1], 'r') as myfile:
  for line in islice(myfile,1,10000):
    if mystring in line.split():
-<<<<<<< HEAD
+#<<<<<<< HEAD
         count=count+line.count("better")
 print float(count) 
-=======
+#=======
 #        total=total+1
 
 #####
 #print total
->>>>>>> 7fe08254a8e3e7ca85d1cea88476b00415d469fd
+#>>>>>>> 7fe08254a8e3e7ca85d1cea88476b00415d469fd
 
