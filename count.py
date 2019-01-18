05a30150d536cd23603545d6505a5c2fcefb58ae
vikram0100 <bikram_bom@yahoo.com>
2019-01-18 01:33:28 +0000

    researcher B script

--- count.py
+++ count.py
@@ -12,9 +12,9 @@ with open(sys.argv[1], 'r') as myfile:
         count=count+line.count("better")
 print float(count) 
 =======
-        total=total+1
+#        total=total+1
 
 #####
-print total
+#print total
 >>>>>>> 7fe08254a8e3e7ca85d1cea88476b00415d469fd
 
