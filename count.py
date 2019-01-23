e2b3ee732ea5a3458f136b4f3b871d3d29920bc1
bikram <bikram_bom@yahoo.com>
2019-01-23 13:31:53 +0000

    Researcher A version of script

--- count.py
+++ count.py
@@ -13,7 +13,7 @@ with open(sys.argv[1], 'r') as myfile:
 print float(count) 
 #=======
 #        total=total+1
-
+############
 #####
 #print total
 #>>>>>>> 7fe08254a8e3e7ca85d1cea88476b00415d469fd
